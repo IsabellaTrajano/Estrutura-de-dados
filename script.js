e386b9b107f6bbb382a0e54662267033265f2dcb
IsabellaTrajano <bellrdapct@gmail.com>
2022-06-09 13:03:43 -0300

    estrutura de dados

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Isabella"
+let numero = 15
+let ano = 2022
+let sub = ano - numero
+let soma = ano + numero
+console.log(soma)
+let text = "Estou mexendo no console.log"
+console.log(text)
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
